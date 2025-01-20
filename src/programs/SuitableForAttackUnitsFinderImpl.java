4ae0a565fa9830f8e6adcb7f34187da74e2dd5e6
Олег Призов <oleg12345prisov@gmail.com>
2025-01-21 01:25:38 +0300

    jar file added

--- src/programs/SuitableForAttackUnitsFinderImpl.java
+++ src/programs/SuitableForAttackUnitsFinderImpl.java
@@ -3,13 +3,44 @@ package programs;
 import com.battle.heroes.army.Unit;
 import com.battle.heroes.army.programs.SuitableForAttackUnitsFinder;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+
+
+// R - количество строк, C - количество элементов в строке
+// обработка всех строк O(R*C)
+
 
 public class SuitableForAttackUnitsFinderImpl implements SuitableForAttackUnitsFinder {
 
     @Override
     public List<Unit> getSuitableUnits(List<List<Unit>> unitsByRow, boolean isLeftArmyTarget) {
-        // Ваше решение
-        return null;
+        List<Unit> suitableUnits = new ArrayList<>();
+        // Для каждой строки в unitsByRow
+        for (List<Unit> row : unitsByRow) {
+            // Найти минимальную или максимальную координату Y в зависимости от isLeftArmyTarget
+            if (isLeftArmyTarget) {
+                // Если атакуется левая армия (компьютер), ищем минимальную координату Y
+                Unit targetUnit = row.stream()
+                        .filter(Unit::isAlive)
+                        .min(Comparator.comparingInt(Unit::getyCoordinate))
+                        .orElse(null);
+                if (targetUnit != null) {
+                    suitableUnits.add(targetUnit);
+                }
+            } else {
+                // Если атакуется правая армия (игрок), ищем максимальную координату Y
+                Unit targetUnit = row.stream()
+                        .filter(Unit::isAlive)
+                        .max(Comparator.comparingInt(Unit::getyCoordinate))
+                        .orElse(null);
+                if (targetUnit != null) {
+                    suitableUnits.add(targetUnit);
+                }
+            }
+        }
+
+        return suitableUnits;
     }
 }
